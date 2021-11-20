41eec686b220d93fb1c68622ed547392a155196e
shoito <sho.ito.1027@gmail.com>
2021-11-20 22:33:08 +0900

    install Firebase core and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/l10n/l10n.dart';
 import 'package:flutter_playground/router/router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(AppRoot());
 }
 
